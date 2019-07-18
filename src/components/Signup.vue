5b8a930c5b6e4011ed81e49eb181edb1e2bd64d8
taramouse <nickykeyse@gmail.com>
2019-07-18 22:47:37 +0100

    Sign up auth added

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -2,15 +2,16 @@
   <div id="signup" class="container">
     <div class="card-panel center">
       <h4>Sign up</h4><br />
-      <form id="signup-form">
+      <form id="signup-form" @submit.prevent="signup">
         <div class="input-field">
-          <input type="email" id="signup-email" required />
+          <input type="email" id="signup-email" v-model="email" required />
           <label for="signup-email">Email address</label>
         </div>
         <div class="input-field">
-          <input type="password" id="signup-password" required />
+          <input type="password" id="signup-password" v-model="password" required />
           <label for="signup-password">Choose password</label>
         </div>
+        <p v-if="feedback" class="red-text">{{ feedback }}</p>
         <button class="btn yellow darken-2 z-depth-0">Sign up</button>
       </form>
     </div>
@@ -18,8 +19,29 @@
 </template>
 
 <script>
+// eslint-disable-next-line
+import { db, auth } from '@/firebase/init'
+
 export default {
-  name: 'Signup'
+  name: 'Signup',
+  data () {
+    return {
+      email: null,
+      password: null,
+      feedback: null
+    }
+  },
+  methods: {
+    signup () {
+      auth.createUserWithEmailAndPassword(this.email, this.password).then(cred => {
+        console.log(cred.user)
+        this.feedback = null
+        this.$router.push({ name: 'Home' })
+      }).catch(err => {
+        this.feedback = err.message
+      })
+    }
+  }
 }
 </script>
 
