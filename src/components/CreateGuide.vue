7ceba321dce98452b4e156d3ed231ca15cb3ab5d
taramouse <nickykeyse@gmail.com>
2019-07-20 22:39:18 +0100

    Now adds new guide to the database

--- src/components/CreateGuide.vue
+++ src/components/CreateGuide.vue
@@ -5,15 +5,16 @@
       <div v-if="!user">
         <p class="red-text">Please log in or sign up to create a guide</p>
       </div>
-      <form v-if="user" id="create-form">
+      <form v-if="user" id="create-form" @submit.prevent="saveGuide">
         <div class="input-field">
-          <input type="text" id="title" required>
+          <input type="text" id="title" v-model="title" required>
           <label for="title">Guide Title</label>
         </div>
         <div class="input-field">
-          <textarea id="content" class="materialize-textarea" required></textarea>
+          <textarea id="content" class="materialize-textarea" v-model="content" required></textarea>
           <label for="content">Guide Content</label>
         </div>
+        <p v-if="feedback" class="red-text">{{ feedback }}</p>
         <button class="btn yellow darken-2 z-depth-0">Create</button>
       </form>
     </div>
@@ -27,12 +28,29 @@ export default {
   name: 'CreateGuide',
   data () {
     return {
-
+      title: '',
+      content: '',
+      feedback: null
     }
   },
   computed: {
     ...mapGetters(['user'])
   },
+  methods: {
+    saveGuide () {
+      this.$db.collection('guides').add({
+        title: this.title,
+        content: this.content
+      }).then(() => {
+        this.title = ''
+        this.content = ''
+        this.feedback = null
+        this.$router.push({ name: 'Home' })
+      }).catch(err => {
+        this.feedback = err.message
+      })
+    }
+  }
 }
 </script>
 
