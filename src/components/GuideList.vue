1a7c253f5ed2ca767855886f8737ef21e044a5b6
taramouse <nickykeyse@gmail.com>
2019-07-20 20:42:29 +0100

    Hide list if user not logged in.

--- src/components/GuideList.vue
+++ src/components/GuideList.vue
@@ -1,27 +1,17 @@
 <template>
   <div id="guide-list" class="container">
-    <ul class="collapsible z-depth-0 guides" style="border: none;">
+    <ul v-if="user" class="collapsible z-depth-0 guides" style="border: none;">
       <li v-for="(guide, index) in guides" :key="index">
         <div class="collapsible-header grey lighten-4">{{ guide.title }}</div>
         <div class="collapsible-body white">{{ guide.content }}</div>
       </li>
-      <!-- <li>
-        <div class="collapsible-header grey lighten-4">Guide title</div>
-        <div class="collapsible-body white"><span>Lorem ipsum dolor sit amet.</span></div>
-      </li>
-      <li>
-        <div class="collapsible-header grey lighten-4">Guide title</div>
-        <div class="collapsible-body white"><span>Lorem ipsum dolor sit amet.</span></div>
-      </li>
-      <li>
-        <div class="collapsible-header grey lighten-4">Guide title</div>
-        <div class="collapsible-body white"><span>Lorem ipsum dolor sit amet.</span></div>
-      </li> -->
     </ul>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
   name: 'GuideList',
   data () {
@@ -29,11 +19,14 @@ export default {
       guides: []
     }
   },
+  computed: {
+    ...mapGetters(['user'])
+  },
   mounted () {
     // Materialize JS accessed via M
-    const items = document.querySelectorAll('.collapsible')
+    const collapsible = document.querySelectorAll('.collapsible')
     // eslint-disable-next-line
-    M.Collapsible.init(items)
+    M.Collapsible.init(collapsible)
   },
   created () {
     this.$db.collection('guides').get().then(snapshot => {
