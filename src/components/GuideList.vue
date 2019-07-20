ae1b2ae3d33409b03afb2d83c3a7c653c9022fbb
taramouse <nickykeyse@gmail.com>
2019-07-20 20:20:24 +0100

    Now get guides from firestore

--- src/components/GuideList.vue
+++ src/components/GuideList.vue
@@ -1,15 +1,11 @@
 <template>
   <div id="guide-list" class="container">
     <ul class="collapsible z-depth-0 guides" style="border: none;">
-      <li>
-        <div class="collapsible-header grey lighten-4">Guide title</div>
-        <div class="collapsible-body white">Lorem ipsum dolor sit amet.</div>
+      <li v-for="(guide, index) in guides" :key="index">
+        <div class="collapsible-header grey lighten-4">{{ guide.title }}</div>
+        <div class="collapsible-body white">{{ guide.content }}</div>
       </li>
-      <li>
-        <div class="collapsible-header grey lighten-4">Guide title</div>
-        <div class="collapsible-body white"><span>Lorem ipsum dolor sit amet.</span></div>
-      </li>
-      <li>
+      <!-- <li>
         <div class="collapsible-header grey lighten-4">Guide title</div>
         <div class="collapsible-body white"><span>Lorem ipsum dolor sit amet.</span></div>
       </li>
@@ -17,6 +13,10 @@
         <div class="collapsible-header grey lighten-4">Guide title</div>
         <div class="collapsible-body white"><span>Lorem ipsum dolor sit amet.</span></div>
       </li>
+      <li>
+        <div class="collapsible-header grey lighten-4">Guide title</div>
+        <div class="collapsible-body white"><span>Lorem ipsum dolor sit amet.</span></div>
+      </li> -->
     </ul>
   </div>
 </template>
@@ -24,11 +24,23 @@
 <script>
 export default {
   name: 'GuideList',
+  data () {
+    return {
+      guides: []
+    }
+  },
   mounted () {
     // Materialize JS accessed via M
     const items = document.querySelectorAll('.collapsible')
     // eslint-disable-next-line
     M.Collapsible.init(items)
+  },
+  created () {
+    this.$db.collection('guides').get().then(snapshot => {
+      snapshot.forEach(doc => {
+        this.guides.push(doc.data())
+      })
+    })
   }
 }
 </script>
