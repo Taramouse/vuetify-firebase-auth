307bfa22bde9868127a539d5a9cffbdbb33399de
taramouse <nickykeyse@gmail.com>
2019-07-21 22:38:47 +0100

    Firebase function addAdminRole added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home container">
     <div class="card-panel center">
       <h2 class="grey-text display-2">Guide List</h2>
+      <AdminActions />
       <GuideList />
     </div>
   </div>
@@ -9,11 +10,13 @@
 
 <script>
 // @ is an alias to /src
+import AdminActions from '@/components/AdminActions'
 import GuideList from '@/components/GuideList'
 
 export default {
   name: 'Home',
   components: {
+    AdminActions,
     GuideList
   }
 }
