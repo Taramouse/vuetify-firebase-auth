6ba3c8f7d1bd2a5e41d34c10093745806a25fcd1
taramouse <nickykeyse@gmail.com>
2019-07-24 19:51:15 +0100

    Ensure only admins can add admins.

--- functions/index.js
+++ functions/index.js
@@ -11,6 +11,12 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 exports.addAdminRole = functions.https.onCall((data, context) => {
+  // check request is made by an admin
+  if (context.auth.token.admin !== true) {
+    return {
+      message: 'Error! You have to be an Admin to add one.'
+    }
+  }
   // get the user and add custom claim (admin)
   return admin.auth().getUserByEmail(data.email).then(user => {
     return admin.auth().setCustomUserClaims(user.uid, {
