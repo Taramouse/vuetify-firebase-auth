1f21045038fbe288117bb347a7466568f6c3e33b
taramouse <nickykeyse@gmail.com>
2019-07-25 12:25:12 +0100

    Added vuetify.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import store from './store'
 import router from './router'
 import FirebasePlugin from '@/firebase/firebasePlugin'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
@@ -12,5 +13,6 @@ Vue.use(FirebasePlugin)
 app = new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
