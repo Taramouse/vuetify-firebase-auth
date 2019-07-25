047b5c588df38dd23a4490dbd4bffa492a76a5f4
taramouse <nickykeyse@gmail.com>
2019-07-25 15:08:33 +0100

    Added vuetify.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
-  <div id="app">
-    <Navbar />
-    <router-view />
-  </div>
+  <v-app id="app">
+    <v-content>
+      <Navbar />
+      <router-view />
+    </v-content>
+  </v-app>
 </template>
 <script>
 import Navbar from '@/components/Navbar'
--- src/main.js
+++ src/main.js
@@ -3,14 +3,14 @@ import App from './App.vue'
 import store from './store'
 import router from './router'
 import FirebasePlugin from '@/firebase/firebasePlugin'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
 Vue.use(FirebasePlugin)
 
 /* eslint-disable no-new */
-app = new Vue({
+new Vue({
   router,
   store,
   vuetify,
