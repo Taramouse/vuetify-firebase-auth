34b57a17e466702197fe7d8f8cedaab34294f182
taramouse <nickykeyse@gmail.com>
2019-07-25 12:10:53 +0100

    Removed materialize css.

--- src/main.js
+++ src/main.js
@@ -2,25 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
-import 'materialize-css/dist/css/materialize.min.css'
-import 'materialize-css/dist/js/materialize.min'
-import firebase from 'firebase/app'
-import 'firebase/auth'
 import FirebasePlugin from '@/firebase/firebasePlugin'
 
 Vue.config.productionTip = false
-let app = null
 
 Vue.use(FirebasePlugin)
 
-firebase.auth().onAuthStateChanged(() => {
-  // create app if not already created
-  /* eslint-disable no-new */
-  if (!app) {
-    app = new Vue({
-      router,
-      store,
-      render: h => h(App)
-    }).$mount('#app')
-  }
-})
+/* eslint-disable no-new */
+app = new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
