2be4ca6f249ff179d1dd7aaa7482795e7dc04c93
taramouse <nickykeyse@gmail.com>
2019-07-30 12:42:38 +0100

    Added user feedback alert component.

---
+++ src/components/UserFeedback.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-alert v-model="show" :closeText="closeText" :color="color" :theme='theme' :dismissible="dismissible" :type="type"></v-alert>
+</template>
+
+<script>
+export default {
+  name: 'UserFeedback',
+  props: {
+    'show': {
+      type: Boolean,
+      default: false
+    },
+    'closeText': {
+      type: String,
+      default: 'Close Alert'
+    },
+    'color': {
+      type: String
+    },
+    'theme': {
+      type: String,
+      dafault: 'light'
+    },
+    'dismissible': {
+      type: Boolean,
+      default: false
+    },
+    'type': {
+      type: String,
+      default: 'info'
+    }
+  }
+}
+</script>
