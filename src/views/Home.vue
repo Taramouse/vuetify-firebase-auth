4eeae1eec8a4ea02b8db865f2a2e8dd1925fb397
taramouse <nickykeyse@gmail.com>
2019-07-31 14:16:02 +0100

    Grey title

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <v-layout text-center
               wrap>
       <v-flex mb-4>
-        <h1 class="display-2 grey-text mb-3">
+        <h1 class="display-2 grey--text mb-3">
           Welcome to Game Guidez
         </h1>
         <p v-if="!user">Please log in or sign up to see our guides</p>
